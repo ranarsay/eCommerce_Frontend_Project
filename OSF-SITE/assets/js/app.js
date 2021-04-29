a5104d3c39cc838f2f9731249d3f3c49795bdb5c
Hilal Rana Anarsay <ranarsay@gmail.com>
2021-04-29 17:52:17 +0300

    featured products and cookies added

--- OSF-SITE/assets/js/app.js
+++ OSF-SITE/assets/js/app.js
@@ -41,3 +41,117 @@ function showSlides(n) {
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
+
+/*featured products slick slide */
+
+$('.autoplay').slick({   
+  slidesToShow: 4,                 
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  dots:true,
+   responsive: [
+     {
+       breakpoint: 992,
+       settings: {
+         slidesToShow: 3
+       }
+     },
+     {
+       breakpoint: 768,
+       settings: {
+         slidesToShow: 2
+       }
+     },
+     {
+       breakpoint: 576,
+       settings: {
+         slidesToShow: 1
+       }
+     }
+   ]
+});
+
+
+
+ // cookies modal start //
+ $('.cookiesModal').modal('show');   /* Showing cookies modal on the page */
+  // cookies modal close //
+
+  
+ 
+
+ // tabs //
+  // tabbed content
+   // http://www.entheosweb.com/tutorials/css/tabs.asp 
+   $(".tab_content").hide();
+   $(".tab_content:first").show();
+
+ /* if in tab mode */
+   $("ul.tabs li").click(function() {
+
+     $(".tab_content").hide();
+     var activeTab = $(this).attr("rel");
+     $("#"+activeTab).fadeIn();
+
+     $("ul.tabs li").removeClass("active");
+     $(this).addClass("active");
+
+   $(".tab_drawer_heading").removeClass("d_active");
+   $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+
+   });
+ /* if in drawer mode */
+ $(".tab_drawer_heading").click(function() {
+
+     $(".tab_content").hide();
+     var d_activeTab = $(this).attr("rel");
+     $("#"+d_activeTab).fadeIn();
+
+   $(".tab_drawer_heading").removeClass("d_active");
+     $(this).addClass("d_active");
+
+   $("ul.tabs li").removeClass("active");
+   $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+   });
+
+
+
+/* cookie popup */
+
+function GetCookie(name) {
+  var arg=name+"=";
+  var alen=arg.length;
+  var clen=document.cookie.length;
+  var i=0;
+  while (i<clen) {
+	var j=i+alen;
+	if (document.cookie.substring(i,j)==arg)
+	  return "here";
+	i=document.cookie.indexOf(" ",i)+1;
+	if (i==0) break;
+  }
+  return null;
+}
+function testFirstCookie(){
+	var offset = new Date().getTimezoneOffset();
+	if ((offset >= -180) && (offset <= 0)) { // European time zones
+		var visit=GetCookie("cookieCompliancyAccepted");
+		if (visit==null){
+		   $("#myCookieConsent").fadeIn(400);	// Show warning
+	   } else {
+			// Already accepted
+	   }		
+	}
+}
+$(document).ready(function(){
+    $("#cookieButton").click(function(){
+		console.log('Understood');
+		var expire=new Date();
+		expire=new Date(expire.getTime()+7776000000);
+		document.cookie="cookieCompliancyAccepted=here; expires="+expire+";path=/";
+        $("#myCookieConsent").hide(400);
+    });
+	testFirstCookie();
+});
+// Cookie  END
